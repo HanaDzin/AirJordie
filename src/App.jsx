8234e0329979a21907542783551e4962c90f3933
HanaDzin <dzinhana@hotmail.com>
2024-09-27 19:37:09 +0200

    basic app structure made

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero section
+    </section>
+    <section className="padding">
+    Popular Products
+    </section>
+    <section className="padding">
+    SuperQuality 
+    </section>
+    <section className="padding-x py-10">
+    Services
+    </section>
+    <section className="padding">
+    SpecialOffer
+    </section>
+    <section className="padding bg-pale-blue">
+    Reviews
+    </section>
+    <section className="padding-x sm:py-32 py-16">
+    Subscribe
+    </section>
+    <section className="padding-x padding-t bg-black pb-8">
+    Footer
+    </section>
+  </main>
+);
+
+export default App;
