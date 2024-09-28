c1c52d83f8fd73826af14615d9642f2363f4d5e7
HanaDzin <dzinhana@hotmail.com>
2024-09-28 13:42:46 +0200

    navbar & hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+import SpecialOffer from "./sections/SpecialOffer";
+import Footer from "./sections/Footer";
+import CustomerReviews from "./sections/CustomerReviews";
+import Services from "./sections/Services";
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero section
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
     </section>
     <section className="padding">
-    Popular Products
+      <PopularProducts />
     </section>
     <section className="padding">
-    SuperQuality 
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-    Services
+      <Services />
     </section>
     <section className="padding">
-    SpecialOffer
+      <SpecialOffer />
     </section>
     <section className="padding bg-pale-blue">
-    Reviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16">
-    Subscribe
+      <Subscribe />
     </section>
     <section className="padding-x padding-t bg-black pb-8">
-    Footer
+      <Footer />
     </section>
   </main>
 );
